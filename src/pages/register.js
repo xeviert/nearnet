0e9b2f0875be9231fecbca70d3068ef0fc7503b8
xeviert <xevierturrubiartes@gmail.com>
2024-06-01 21:24:18 +0200

    installed js cookies pakages

--- src/pages/register.js
+++ src/pages/register.js
@@ -55,7 +55,7 @@ export default function Register() {
           </div>
           <button
             type="submit"
-            className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
           >
             Register
           </button>
