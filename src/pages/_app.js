aaaed278d84aa963c5bc793bc50a96a417f7515b
xeviert <xevierturrubiartes@gmail.com>
2024-06-06 17:03:36 +0200

    favicon fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../app/globals.css';
+import Head from 'next/head'; // Import Head
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import { UserProvider } from '../context/UserContext';
@@ -8,6 +9,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <FavorProvider>
+        <Head>
+          <title>NearNet</title>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        </Head>
         <div className="flex flex-col min-h-screen">
           <NavBar />
           <main className="flex-grow pt-16">
